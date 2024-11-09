3fd839daaaa4043e67582e61b8d7369fa0708862
AayushMohan <aayushmohan1702@gmail.com>
2024-11-09 16:38:54 +0530

    Add Header component and integrate it into the layout; update Home page with a button

---
+++ components/Header.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+
+function Header() {
+  const { user } = useUser();
+  console.log(user);
+
+  return (
+    <header className="flex flex-wrap justify-between items-center px-4 py-2">
+      {/* Top Row */}
+      <div>
+        <Link
+          href="/"
+          className="text-2xl font-bold text-blue-500 hover:opacity-50 cursor-pointer mx-auto sm:mx-0"
+        >
+          StellarCart
+        </Link>
+      </div>
+    </header>
+  );
+}
+export default Header;
