f037f3d732354d673fad6f429602285308313b34
AayushMohan <aayushmohan1702@gmail.com>
2025-01-07 15:26:54 +0530

    Implement product detail page and add to basket functionality

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import AddToBasketButton from "@/components/AddToBasketButton";
 import { imageUrl } from "@/lib/imageUrl";
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { PortableText } from "next-sanity";
@@ -54,6 +55,10 @@ const ProductPage = async ({
                 <PortableText value={product.description} />
               )}
             </div>
+          </div>
+
+          <div className="mt-6">
+            <AddToBasketButton product={product} disabled={isOutOfStock} />
           </div>
         </div>
       </div>
